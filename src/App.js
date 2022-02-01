5f623255aaf3ac450d1c8c3a12f767bea1d5c53c
SimeoneVilardo <simeone.vilardo@gmail.com>
2022-02-01 23:52:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import './App.css';
+
+function App() {
+  const [url, setUrl] = useState('');
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    alert(`Submitting Name ${url}`)
+  }
+  return (
+    <div className="App">
+      <div className="limiter">
+        <div className="container-login100">
+          <div className="wrap-login100">
+
+            <form className="login100-form validate-form" onSubmit={handleSubmit}>
+              <span className="login100-form-title">
+                URL Shortener
+              </span>
+
+              <div className="wrap-input100 validate-input">
+                <input className="input100" type="text" name="url" placeholder="Url" value={url} onChange={e => setUrl(e.target.value)}></input>
+                <span className="focus-input100"></span>
+                <span className="symbol-input100">
+                  <i className="fa fa-link" aria-hidden="true"></i>
+                </span>
+              </div>
+
+              <div className="container-login100-form-btn">
+                <input className="login100-form-btn" value="Short" type="submit"></input>
+              </div>
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
